8a029f420e32f990dc6b96c6b2f31cdb5a134adf
sandeepsab <89964039+sandeepsab@users.noreply.github.com>
2021-09-06 10:17:23 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -7,10 +7,10 @@ class Translation(object):
     START_TEXT = """<b>Hey {}!!</b>
 <i>Am Just A Advance Auto Filter Bot....😉
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+Just Add Me 
 
 For More Details Click Help Button Below..
-@CrazyBotsz
+@movie_club_arena
 </i>"""    
     
     HELP_TEXT = """
